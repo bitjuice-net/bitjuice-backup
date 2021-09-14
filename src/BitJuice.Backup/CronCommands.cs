9fa5d6606d4c0527820fe4184270a0af20a402b4
Jarosław Dutka <jaroslawjdutka@gmail.com>
2021-09-14 14:00:36 +0200

    Fix cron install

--- src/BitJuice.Backup/CronCommands.cs
+++ src/BitJuice.Backup/CronCommands.cs
@@ -1,25 +1,24 @@
-﻿using System.CommandLine.Invocation;
+﻿using System;
+using System.CommandLine.Invocation;
 using System.IO;
-using System.Reflection;
 
 namespace BitJuice.Backup
 {
     public class CronCommands
     {
-        private static string CronFilePath = "/etc/cron.daily/bitjuice-backup";
+        private static readonly string CronFilePath = "/etc/cron.daily/bitjuice-backup";
+        private static string BaseDirectory => AppContext.BaseDirectory;
+        private static string ExecutableName => AppDomain.CurrentDomain.FriendlyName;
 
         public static void Install()
         {
-            var path = new FileInfo(Assembly.GetExecutingAssembly().Location);
-            var dir = path.Directory?.FullName;
-
             var cron = new FileInfo(CronFilePath);
             using (var writer = new StreamWriter(cron.Create()))
             {
                 writer.WriteLine("#!/bin/bash");
                 writer.WriteLine();
-                writer.WriteLine($"cd {dir}");
-                writer.WriteLine("./BitJuice.Backup execute");
+                writer.WriteLine($"cd {BaseDirectory}");
+                writer.WriteLine($"./{ExecutableName} execute");
             }
 
             Process.StartProcess("chmod", $"+x {cron.FullName}");
