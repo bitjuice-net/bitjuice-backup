7dd87a060b627bb901f57f98cbfc4efa976af878
Jarosław Dutka <jaroslawjdutka@gmail.com>
2021-09-14 14:43:10 +0200

    Add Tar aggregator

---
+++ src/BitJuice.Backup.Modules/Aggregators/TarAggregator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BitJuice.Backup.Infrastructure;
+
+namespace BitJuice.Backup.Modules.Aggregators
+{
+    [ModuleName("tar-aggregator")]
+    public class TarAggregator : ArchiveAggregator<TarBuilder>
+    {
+        public TarAggregator(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
---
+++ src/BitJuice.Backup.Modules/Aggregators/TarBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using BitJuice.Backup.Model;
+using ICSharpCode.SharpZipLib.Tar;
+using Microsoft.Extensions.Logging;
+
+namespace BitJuice.Backup.Modules.Aggregators
+{
+    public class TarBuilder : ArchiveBuilder
+    {
+        private TarOutputStream tarStream;
+
+        public TarBuilder(ILogger<ArchiveBuilder> logger, IEnumerable<IDataItem> items) : base(logger, items)
+        {
+        }
+
+        protected override void OpenArchive()
+        {
+            tarStream = new TarOutputStream(OutputStream, Encoding.UTF8) { IsStreamOwner = false };
+        }
+
+        protected override void OpenFile()
+        {
+            var tarEntry = TarEntry.CreateTarEntry(CurrentItem.VirtualPath);
+            tarEntry.Size = CurrentItemStream.Length;
+            tarStream.PutNextEntry(tarEntry);
+        }
+
+        protected override void WriteFile(byte[] buffer, int count)
+        {
+            tarStream.Write(buffer, 0, count);
+        }
+
+        protected override void CloseFile()
+        {
+            tarStream.CloseEntry();
+        }
+
+        protected override void CloseArchive()
+        {
+            tarStream.Finish();
+            tarStream.Close();
+            tarStream = null;
+        }
+    }
+}
