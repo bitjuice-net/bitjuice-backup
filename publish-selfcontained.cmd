69f0b0c1ad468cda8b61efd0b67551443ecc72fb
Jarosław Dutka <jaroslawjdutka@gmail.com>
2024-08-20 21:02:33 +0200

    Update command

--- publish-selfcontained.cmd
+++ publish-selfcontained.cmd
@@ -1,2 +1,5 @@
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -o publish/linux-x64 src\BitJuice.Backup\BitJuice.Backup.csproj
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o publish/win-x64 src\BitJuice.Backup\BitJuice.Backup.csproj
+dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -o publish/linux-x64-sc src\BitJuice.Backup\BitJuice.Backup.csproj
+dotnet publish -c Release -r linux-x64 --self-contained false -p:PublishSingleFile=true -o publish/linux-x64 src\BitJuice.Backup\BitJuice.Backup.csproj
+
+dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o publish/win-x64-sc src\BitJuice.Backup\BitJuice.Backup.csproj
+dotnet publish -c Release -r win-x64 --self-contained false -p:PublishSingleFile=true -o publish/win-x64 src\BitJuice.Backup\BitJuice.Backup.csproj
